3bdff9c9bee1f259df260234b935062183fd6f5e
chika ristysuatantri <chikaristysuatantri@gmail.com>
2025-12-18 12:35:20 +0700

    pendahuluan

--- pages/page1.py
+++ pages/page1.py
@@ -1,4 +1,72 @@
 import streamlit as st
 
-st.title("Welcome to Chika's Home page")
-st.write("Ini demo aplikasi streamlit.")
+st.title("Analisis Pertumbuhan Ekonomi Indonesia")
+st.subheader("PENDAHULUAN")
+
+import streamlit as st
+
+st.markdown(
+    """
+    <div style="text-align: justify; line-height: 1.6;">
+    Pertumbuhan ekonomi merupakan salah satu indikator dalam menentukan keberhasilan
+    suatu negara yang ditinjau dari bertambahnya produksi barang industri,
+    berkembangnya infrastruktur, bertambahnya sekolah, serta bertambahnya produksi
+    barang modal dan bertambahnya sektor jasa. Menurut Boediono (2012), pertumbuhan
+    ekonomi diartikan sebagai proses kenaikan output per kapita dalam jangka panjang.
+    Menurutnya, pertumbuhan ekonomi ditekankan pada tiga aspek utama, yaitu proses,
+    output per kapita, dan jangka panjang.
+
+    <br><br>
+    Seiring berjalannya waktu, kondisi perekonomian Indonesia mengalami fluktuasi yang
+    berarti. Sejak tahun 1965 hingga tahun 1997, Indonesia memiliki pertumbuhan
+    ekonomi yang cukup baik dengan rata-rata pertumbuhan per tahunnya mencapai
+    angka 7 persen. Namun krisis keuangan yang melanda Asia pada akhir tahun 1990-an
+    memberikan dampak negatif bagi pertumbuhan perekonomian negara-negara Asia,
+    salah satunya Indonesia. Sebagai salah satu negara yang terkena dampak krisis
+    keuangan tersebut, pertumbuhan ekonomi Indonesia yang dilihat dari Produk
+    Domestik Bruto (PDB) menurun hingga 13,6 persen pada tahun 1998 dan kemudian
+    naik sebesar 0,3 persen pada tahun 1999.
+
+    <br><br>
+    Setelah terjadinya krisis keuangan Asia tersebut, pertumbuhan ekonomi Indonesia
+    mulai pulih dengan rata-rata PDB sebesar 4,6 persen per tahunnya dari tahun 2000
+    hingga 2004. Setelah tahun 2004, pertumbuhan ekonomi Indonesia terus mengalami
+    perbaikan dan angka PDB mencapai sekitar 6 persen per tahunnya, kecuali pada
+    tahun 2009 dan 2013 yang turun menjadi masing-masing 4,6 persen dan 5,8 persen
+    ketika terjadi krisis keuangan global.
+
+    <br><br>
+    Menurut Todaro (1994), terdapat tiga komponen pertumbuhan ekonomi yang memiliki
+    arti penting bagi setiap masyarakat, yaitu akumulasi modal yang meliputi semua
+    investasi baru dalam bentuk tanah, peralatan fisik, serta sumber daya manusia
+    melalui perbaikan di bidang kesehatan, pendidikan, dan keterampilan kerja.
+
+    <br><br>
+    Teori pertumbuhan ekonomi Harrod-Domar merupakan teori pertumbuhan yang paling
+    sederhana yang menggambarkan perekonomian secara sederhana. Dalam teori ini,
+    akumulasi modal dan tenaga kerja digunakan sebagai faktor utama yang memengaruhi
+    pertumbuhan ekonomi suatu negara, dengan asumsi tingkat suku bunga dianggap
+    konstan atau tetap. Selanjutnya, Solow mengembangkan teori Harrod-Domar dengan
+    mengasumsikan bahwa tingkat suku bunga dapat berubah atau tidak konstan.
+    Perubahan tingkat suku bunga tersebut akan memengaruhi tabungan dan investasi
+    dalam masyarakat. Teori Solow mampu menggambarkan pertumbuhan ekonomi pada
+    negara berkembang dengan menggunakan kombinasi akumulasi modal dan tenaga kerja.
+
+    <br><br>
+    Selain itu, Solow juga menambahkan faktor teknologi sebagai variabel yang
+    memengaruhi pertumbuhan ekonomi suatu negara. Teori lain yang menjelaskan
+    pertumbuhan ekonomi adalah teori Endogen yang dikemukakan oleh Romer. Teori ini
+    berpendapat bahwa akumulasi modal merupakan faktor penting dalam menentukan
+    pertumbuhan ekonomi, namun lebih menekankan pada peningkatan kualitas sumber
+    daya manusia (skill) dan penggunaan teknologi melalui penelitian dan
+    pengembangan (R&D). Berdasarkan kedua teori tersebut, penulis ingin melihat
+    kondisi pertumbuhan ekonomi Indonesia saat ini.
+
+    <br><br>
+    Berdasarkan uraian latar belakang tersebut, maka tujuan dari penelitian ini
+    adalah: (1) menganalisis perkembangan kondisi perekonomian dan tenaga kerja di
+    Indonesia; dan (2) menganalisis pada kondisi mana perekonomian Indonesia saat ini.
+    </div>
+    """,
+    unsafe_allow_html=True
+)
