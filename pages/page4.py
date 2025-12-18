3b7cbb332c8a97b708550d7b34d25af680e46140
chika ristysuatantri <chikaristysuatantri@gmail.com>
2025-12-18 12:59:03 +0700

    pendahuluan

--- pages/page4.py
+++ pages/page4.py
@@ -7,7 +7,6 @@ st.markdown(
     """
     <div style="text-align: justify; line-height: 1.6;">
     <b>Kesimpulan</b>
-
     <br>
     1. Variabel bebas yang dianalisis menunjukkan bahwa variabel yang berpengaruh
     signifikan terhadap output (PDB) adalah variabel modal (K). Sementara itu,
@@ -19,7 +18,7 @@ st.markdown(
     sarjana atas dasar harga konstan yang menunjukkan pengaruh positif, namun
     tetap tidak signifikan secara statistik.
 
-    <br><br>
+    <br>
     2. Hasil pengujian terhadap empat model menunjukkan bahwa jumlah seluruh
     koefisien regresi lebih kecil dari satu. Pada perhitungan atas dasar harga
     berlaku, masing-masing koefisien bernilai sebesar 0,7292 dan 0,3527.
@@ -28,38 +27,38 @@ st.markdown(
     menunjukkan bahwa tingkat pengembalian skala bersifat menurun (decreasing
     return to scale).
 
-    <br><br>
+    <br>
     3. Berdasarkan dua hasil analisis tersebut, dapat disimpulkan bahwa Indonesia
     saat ini berada pada tahap awal menuju Era Long Run Economic Growth.
 
     <br><br>
     <b>Saran</b>
 
-    <br><br>
+    <br>
     Dalam upaya mempercepat Indonesia menuju Era Long Run Economic Growth,
     terdapat beberapa kebijakan yang dapat dijadikan sebagai masukan, antara lain:
 
-    <br><br>
+    <br>
     1. Meningkatkan anggaran investasi di bidang pendidikan, khususnya dalam
     pengembangan keterampilan (skill) dan kegiatan penelitian serta pengembangan
     (R&D).
 
-    <br><br>
+    <br>
     2. Memperbaiki mutu dan kualitas pendidikan, baik dari segi infrastruktur
     sarana dan prasarana maupun kualitas tenaga pengajar yang berbasis pada
     keahlian serta kebutuhan pasar global, guna menciptakan tenaga kerja terampil
     yang baru.
 
-    <br><br>
+    <br>
     3. Meningkatkan keterampilan tenaga kerja melalui jalur pendidikan nonformal
     dalam bentuk pelatihan-pelatihan untuk meningkatkan kualitas tenaga kerja
     yang sudah ada.
 
-    <br><br>
+    <br>
     4. Meningkatkan jumlah tenaga kerja terdidik dan terampil dengan tingkat
     pendidikan tinggi.
 
-    <br><br>
+    <br>
     5. Meningkatkan kerja sama dengan perguruan tinggi dalam menciptakan inovasi
     terbaru, khususnya di bidang riset dan pengembangan teknologi serta
     peningkatan kualitas kelembagaan.
